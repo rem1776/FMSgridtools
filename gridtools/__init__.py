1031b3c86bb694078024085a063671b5ffec5f53
Ryan Mulhall <35538242+rem1776@users.noreply.github.com>
2025-01-27 16:08:40 -0500

    add get_provenance_attrs util function for global attributes (#20)
    Co-authored-by: rem1776 <Ryan.Mulhall@noaa.gov>

--- gridtools/__init__.py
+++ gridtools/__init__.py
@@ -1,5 +1,6 @@
 from .shared.gridobj import GridObj
 from .make_topog.topogobj import TopogObj
 from .shared.gridtools_utils import check_file_is_there
+from .shared.gridtools_utils import get_provenance_attrs
 from .shared.mosaicobj import MosaicObj
 from .shared.xgridobj import XGridObj
