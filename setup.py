9b1c712b2e3c4f551f96661db83bee4d1d512a77
MiKyung Lee <58964324+mlee03@users.noreply.github.com>
2025-02-13 15:21:48 -0500

    remove unapproved pylib files, cleanup cfrenctools (#24)
    Co-authored-by: mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ class CustomInstall(install):
         with open("compile_log.txt", "w") as f:
             subprocess.run(
                 ["cmake", ".."], 
-                cwd="./FRENCTools_lib/cfrenctools/c_build", 
+                cwd="./FREnctools_lib/cfrenctools/c_build", 
                 stdout=f,
                 stderr=subprocess.STDOUT,
             )
             subprocess.run(
                 ["make"], 
-                cwd="./FRENCTools_lib/cfrenctools/c_build", 
+                cwd="./FREnctools_lib/cfrenctools/c_build", 
                 stdout=f,
                 stderr=subprocess.STDOUT,
             )
@@ -48,7 +48,7 @@ setup(
     extras_require=extras_requires,
     name="fmsgridtools",
     license="",
-    packages=find_namespace_packages(include=["FMSgridtools", "FMSgridtools.*", "FRENCTools_lib", "FRENCTools_lib.pyfrenctools.*"]),
+    packages=find_namespace_packages(include=["FMSgridtools", "FMSgridtools.*", "FREnctools_lib", "FREnctools_lib.pyfrenctools.*"]),
     include_package_data=True,
     version="0.0.1",
     zip_safe=False,
