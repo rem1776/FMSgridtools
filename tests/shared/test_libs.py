1462ed1710e6b3cd2c49dbd0fa1d0783b6bc8040
rem1776 <Ryan.Mulhall@noaa.gov>
2025-02-20 15:20:49 -0500

    fix path in link test

--- tests/shared/test_libs.py
+++ tests/shared/test_libs.py
@@ -2,5 +2,5 @@ import ctypes
 
 # simple test to check if C libraries can be linked in without error 
 def test_c_libs():
-    libfile = "./FRENCTools_lib/cfrenctools/c_build/clib.so"
+    libfile = "./FREnctools_lib/cfrenctools/c_build/clib.so"
     c_lib = ctypes.CDLL(libfile)
